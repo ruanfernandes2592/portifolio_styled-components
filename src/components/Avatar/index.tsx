92d5e09a374dfab939324acb22ac2171d60b5d5d
ruanfernandes2592 <ruanfernandes2592@gmail.com>
2024-12-17 23:40:48 -0300

    Estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/ruanfernandes2592.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ruanfernandes2592.png" alt="" />
-    <Titulo fontSize={20}>Ruan Fernandes</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Ruan Fernandes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={12}>
+        ruanfernandes2592
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={14}>
+        Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
